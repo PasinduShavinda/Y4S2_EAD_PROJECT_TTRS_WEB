87b4aab2a53c22f8cac8758858061df90a446f6f
Kalansooriya S. H <it20137700@my.sliit.lk>
2023-10-06 19:03:02 +0530

    HomePage Added

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
+import Homepage from "./components/HomePage/Homepage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <React.Fragment>
+    
+        <Routes>
+        <Route path="/" element={< Homepage />} exact />
+        </Routes>
+        </React.Fragment>
     </div>
   );
 }
